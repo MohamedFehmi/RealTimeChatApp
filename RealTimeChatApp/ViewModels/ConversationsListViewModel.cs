32a4140591bf9a4aa861543d47aaeb57ddd4f672
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-19 21:03:17 +0100

    Added ConversationsListViewModel

---
+++ RealTimeChatApp/ViewModels/ConversationsListViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace RealTimeChatApp.ViewModels
+{
+    public class ConversationsListViewModel : INotifyPropertyChanged
+    {
+        //Properties
+        public ObservableCollection<ConversationOverview> Conversations { get; set; }
+
+        public ConversationsListService conversationsListService;
+
+        public ConversationsListViewModel()
+        {
+            conversationsListService = new ConversationsListService();
+            Conversations = conversationsListService.getAllConversations();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
