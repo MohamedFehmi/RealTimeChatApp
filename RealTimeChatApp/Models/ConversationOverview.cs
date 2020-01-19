b39e4e695cd6c4d5ee5c75b3ad2e59d282e624d1
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-19 21:04:27 +0100

    Added ConversationOverview model

---
+++ RealTimeChatApp/Models/ConversationOverview.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace RealTimeChatApp.Models
+{
+    public class ConversationOverview
+    {
+        [JsonProperty("Id")]
+        public string Id { get; set; }
+
+        [JsonProperty("Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Image")]
+        public string Image { get; set; }
+
+        [JsonProperty("MembersCount")]
+        public int MembersCount { get; set; }
+
+        [JsonProperty("LastMessage")]
+        public string LastMessage { get; set; }
+
+        [JsonProperty("LastMessageDate")]
+        public string LastMessageDate { get; set; }
+
+        [JsonProperty("UnreadMessagesCount")]
+        public int UnreadMessagesCount { get; set; }
+    }
+}
