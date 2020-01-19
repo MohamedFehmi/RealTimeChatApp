b7dbf939e21adedbe76488619d56e2944afd826f
Mohamed Fehmi <mohamed.fehmi@actiworks.de>
2020-01-19 21:00:32 +0100

    Added ConversationsListPage

---
+++ RealTimeChatApp/Views/Pages/Chat/ConversationsListPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace RealTimeChatApp.Views.Pages.Chat
+{
+    public partial class ConversationsListPage : ContentPage
+    {
+        public ConversationsListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
